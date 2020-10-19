e597d65f0ac6c95710fda610bf79aecf6f01cd5d
AAVN\hvhai <m.hovanhai@gmail.com>
2020-10-19 17:40:42 +0700

    add log4j

--- adapters/web/src/main/java/com/codehunter/countdowntimer/ca/adapter/web/controller/CreateEventController.java
+++ adapters/web/src/main/java/com/codehunter/countdowntimer/ca/adapter/web/controller/CreateEventController.java
@@ -3,6 +3,7 @@ package com.codehunter.countdowntimer.ca.adapter.web.controller;
 import com.codehunter.countdowntimer.ca.persistence.WebAdapter;
 import com.codehunter.countdowntimer.ca.core.port.in.CreateEventUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CreateEventController {
     private final CreateEventUseCase createEventUseCase;
 
     @PostMapping(path = "/event")
     @ResponseBody
     void createEvent(@RequestBody CreateEventWebDataIn createEventWebDataIn){
+        log.info("Create new event {} ", createEventWebDataIn);
         CreateEventUseCase.CreateEventIn in = new CreateEventUseCase.CreateEventIn(
                 createEventWebDataIn.getName(),
                 createEventWebDataIn.getEventTime()
