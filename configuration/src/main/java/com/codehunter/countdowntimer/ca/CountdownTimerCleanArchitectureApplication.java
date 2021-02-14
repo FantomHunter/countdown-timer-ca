812c7fdbeeb6ca204d0d050483fdf0f212bfede6
codehunter <hunterx6323@gmail.com>
2021-02-14 22:05:18 +0700

    add cors filter without spring security

--- configuration/src/main/java/com/codehunter/countdowntimer/ca/CountdownTimerCleanArchitectureApplication.java
+++ configuration/src/main/java/com/codehunter/countdowntimer/ca/CountdownTimerCleanArchitectureApplication.java
@@ -2,11 +2,24 @@ package com.codehunter.countdowntimer.ca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CountdownTimerCleanArchitectureApplication {
     public static void main(String[] args) {
         SpringApplication.run(CountdownTimerCleanArchitectureApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
