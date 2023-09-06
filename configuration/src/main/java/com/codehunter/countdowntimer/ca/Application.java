9a80b4c3a91577b423355152b7e02ffe3aa1386a
hvhai <hunterx6323@gmail.com>
2023-09-06 09:43:37 +0700

    chore: change App main class

--- configuration/src/main/java/com/codehunter/countdowntimer/ca/Application.java
+++ configuration/src/main/java/com/codehunter/countdowntimer/ca/Application.java
@@ -2,14 +2,11 @@ package com.codehunter.countdowntimer.ca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CountdownTimerCleanArchitectureApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(CountdownTimerCleanArchitectureApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 //    @Bean
