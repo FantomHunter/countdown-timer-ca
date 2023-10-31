a81b93a57f3645df1b3528fa575b6b10cceb42ac
hvhai <hunterx6323@gmail.com>
2023-10-31 17:31:06 +0700

    fix: add cors

--- configuration/src/main/java/com/codehunter/countdowntimer/ca/security/WebMvcConfig.java
+++ configuration/src/main/java/com/codehunter/countdowntimer/ca/security/WebMvcConfig.java
@@ -8,11 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
-                .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS" );
-
+        registry.addMapping("/**");
     }
 }
